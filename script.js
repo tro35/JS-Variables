63f8a301259be7f2382017b50d2586e94303a8ec
tro35 <tomrol@email.cz>
2020-10-25 17:11:48 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,84 @@
+let num1 = 2020;
+undefined
+let num2 = 1975;
+undefined
+let age = num1 - num2;
+undefined
+age
+45
+
+
+let num1 = 1945;
+undefined
+let num2 = 1939;
+undefined
+let ww2 = num1 - num2;
+undefined
+ww2
+6
+
+let tesla = 460.2;
+undefined
+tesla
+460.2
+
+let hour = 3600;
+undefined
+let secondsOfDay = hour * 24;
+undefined
+secondsOfDay
+86400
+
+let people = 7594000000;
+undefined
+let hungary = 9773000;
+undefined
+let hun1 = people / 100 ;
+undefined
+let hungarians = hungary / hun1;
+undefined
+hungarians
+0.1286937055570187
+
+let cina = 9597000;
+undefined
+let cz = 78866;
+let temp = cz / cina * 100; 
+undefined
+temp
+0.8217776388454726
+bigger = 100 - temp;
+99.17822236115452
+
+let people = 654;
+undefined
+let room = 7;
+undefined
+let participants = people % room;
+undefined
+participants
+3
+
+let names = ['Tomas', 'Jana', 'Klara'];
+undefined
+let name = names[2].length > 5;
+undefined
+name
+false
+
+names.push('Petra');
+4
+names[0] = 'Tomas';
+"Tomas"
+let result = names.length > 4;
+undefined
+result
+false
+
+let container = { colors:['red', 'yellow', 'blue', 'green'] , hasManyColors: ['white', 'purple']}
+console.log(container.colors)
+(4) ["red", "yellow", "blue", "green"]
+undefined
+console.log(container.hasManyColors.length > 3)
+false
+undefined
